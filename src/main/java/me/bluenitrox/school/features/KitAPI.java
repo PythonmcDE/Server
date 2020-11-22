f42bc848d706cd6ad082b9ba31496c773a8a3a60
nitrox_blue <fehennerich@outlook.de>
2020-11-22 17:52:48 +0100

    KitAPI update KitItems update

--- src/main/java/me/bluenitrox/school/features/KitAPI.java
+++ src/main/java/me/bluenitrox/school/features/KitAPI.java
@@ -3,13 +3,14 @@ package me.bluenitrox.school.features;
 import me.bluenitrox.school.utils.ItemBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class KitAPI {
 
-    private final String guiname = "§f§lKit Menu";
+    private final String guiname = "§6§lDeine Kits";
 
     public void openKitMenu(Player p){
         Inventory inv = Bukkit.createInventory(null, 9*6,guiname);
@@ -21,9 +22,52 @@ public class KitAPI {
 
     private void setKitContent(Inventory inv){
         ItemStack glas = new ItemBuilder(Material.STAINED_GLASS_PANE).setDisplayname(" ").build();
+        ItemStack blackglass = new ItemBuilder(Material.STAINED_GLASS_PANE,  (short)15).setDisplayname(" ").build();
+
+        for(int i = 0; i != 10; i++){
+            inv.setItem(i,glas);
+        }
+
+        inv.setItem(9, new ItemBuilder(Material.WOOD_SWORD).setDisplayname("§6§lHolz").setLore("§8» §7Du besitzt dieses Kit, klicke", "§8» §7hier, um es §a§lAbzuholen§7.").addEnchant(Enchantment.ARROW_INFINITE, 10, false).build());
+        inv.setItem(10, new ItemBuilder(Material.STONE_SWORD).setDisplayname("§c§o§lStein").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l35 mal §7ab!").build());
+        inv.setItem(11, new ItemBuilder(Material.IRON_SWORD).setDisplayname("§c§o§lEisen").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l50 mal §7ab!").build());
+        inv.setItem(12, new ItemBuilder(Material.BOW).setDisplayname("§c§o§lWarzone").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l50 mal §7ab!").build());
+        inv.setItem(13, new ItemBuilder(Material.DIAMOND_SWORD).setDisplayname("§c§o§lDiamant").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l50 mal §7ab!").build());
+
+        for(int in2 = 14; in2 != 17; in2++){
+            inv.setItem(in2,blackglass);
+        }
+
+        inv.setItem(18, new ItemBuilder(Material.DIAMOND_PICKAXE).setDisplayname("§c§o§lBergarbeiter").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Kaufpreis: §61 Mio Gems").build());
+        inv.setItem(19, new ItemBuilder(Material.GOLD_INGOT).setDisplayname("§c§o§lGoldfinger").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l35 mal §7ab!").build());
+        inv.setItem(20, new ItemBuilder(Material.DIAMOND_SWORD).setDisplayname("§c§o§lJuwelier").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l50 mal §7ab!").build());
+        inv.setItem(21, new ItemBuilder(Material.EMERALD).setDisplayname("§c§o§lBänker").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l50 mal §7ab!").build());
+
+        for(int in3 = 22; in3 != 26; in3++){
+            inv.setItem(in3,blackglass);
+        }
+
+        inv.setItem(27, new ItemBuilder(Material.MELON).setDisplayname("§c§o§lNinja").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Kaufpreis: §65 Mio Gems").build());
+        inv.setItem(28, new ItemBuilder(Material.SULPHUR).setDisplayname("§c§o§lSensei").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l35 mal §7ab!").build());
+        inv.setItem(29, new ItemBuilder(Material.GOLD_CHESTPLATE).setDisplayname("§c§o§lMeister").setLore("§8» §7Noch §cnicht freigeschaltet§7!", "§8» §7Hole das §6vorherige Kit §7noch §6§l50 mal §7ab!").build());
+
+        for(int in4 = 30; in4 != 35; in4++){
+            inv.setItem(in4,blackglass);
+        }
+        for(int in5 = 36; in5 != 45; in5++){
+            inv.setItem(in5,glas);
+        }
+        for(int in6 = 46; in6 != 49; in6++){
+            inv.setItem(in6,glas);
+        }
+
+        inv.setItem(50, new ItemBuilder(Material.PAPER).setDisplayname("§8» §6§lKit Menu").setLore("§6▶ §7Hole dir §6deine Kits §7ab!", "", "§7Kits können durch", "§8● §6das Erwerben des gewünschten Kits §7 oder", "§8● §6wiederholtes Abholen der vorherigen Kits", "§7freigeschaltet werden!").build());
 
         /*TODO
             inv.setItem(Slot, Item);
+            for(int i = 0; i != 10; i++){
+                inv.setItem(i,glas);
+            }
             für jedes item
             Kit Menu bitte genau so machen wie auf euer Screenshot
             ACHTE AUF RECHTSCHREIBUNG UND RICHTIGE BENENNUNG!!
--- src/main/java/me/bluenitrox/school/features/KitItems.java
+++ src/main/java/me/bluenitrox/school/features/KitItems.java
@@ -3,6 +3,7 @@ package me.bluenitrox.school.features;
 import me.bluenitrox.school.utils.ItemBuilder;
 import net.minecraft.server.v1_8_R3.ItemBanner;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 
 public class KitItems {
@@ -19,8 +20,129 @@ public class KitItems {
 
      */
 
-    void addKITNAMEItems(Player p){
-        p.getInventory().addItem(new ItemBuilder(Material.STONE).setDisplayname("TEST ITEM").build());
+    void addHolzItems(Player p){
+        p.getInventory().addItem(new ItemBuilder(Material.WOOD_SWORD).setDisplayname("§7Holzschwert").build());
+        p.getInventory().addItem(new ItemBuilder(Material.WOOD_PICKAXE).setDisplayname("§7Holzspitzhacke").build());
+        p.getInventory().addItem(new ItemBuilder(Material.WOOD_AXE).setDisplayname("§7Holzaxt").build());
+        p.getInventory().addItem(new ItemBuilder(Material.WOOD_SPADE).setDisplayname("§7Holzschaufel").build());
+        p.getInventory().addItem(new ItemBuilder(Material.APPLE).setDisplayname("§7Apfel").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_HELMET).setDisplayname("§7Lederhelm").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_CHESTPLATE).setDisplayname("§7Lederbrustplatte").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_LEGGINGS).setDisplayname("§7Lederhose").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_BOOTS).setDisplayname("§7Lederschuhe").build());
+    }
+    void addSteinItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.STONE_SWORD).setDisplayname("§7Steinschwert").build());
+        p.getInventory().addItem(new ItemBuilder(Material.STONE_PICKAXE).setDisplayname("§7Steinspitzhacke").build());
+        p.getInventory().addItem(new ItemBuilder(Material.STONE_AXE).setDisplayname("§7Steinaxt").build());
+        p.getInventory().addItem(new ItemBuilder(Material.STONE_SPADE).setDisplayname("§7Steinschaufel").build());
+        p.getInventory().addItem(new ItemBuilder(Material.BREAD).setDisplayname("§7Brot").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_HELMET).setDisplayname("§7Goldhelm").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_CHESTPLATE).setDisplayname("§7Goldbrustplatte").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_LEGGINGS).setDisplayname("§7Goldhose").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_BOOTS).setDisplayname("§7Goldschuhe").build());
+    }
+    void addEisenItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_SWORD).setDisplayname("§7Eisenschwert").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_PICKAXE).setDisplayname("§7Eisenspitzhacke").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_AXE).setDisplayname("§7Eisenaxt").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_SPADE).setDisplayname("§7Eisenschaufel").build());
+        p.getInventory().addItem(new ItemBuilder(Material.COOKED_BEEF).setDisplayname("§7Steak").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_HELMET).setDisplayname("§7Eisenhelm").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_CHESTPLATE).setDisplayname("§7Eisenbrustplatte").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_LEGGINGS).setDisplayname("§7Eisenhose").build());
+        p.getInventory().addItem(new ItemBuilder(Material.IRON_BOOTS).setDisplayname("§7Eisenschuhe").build());
+    }
+    void addWarzoneItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.BOW).setDisplayname("§7Bogen").build());
+        p.getInventory().addItem(new ItemBuilder(Material.ARROW).setDisplayname("§7Pfeile").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.POTION, (short) 8194).setDisplayname("§7Trank der Schnelligkeit").build());
+        p.getInventory().addItem(new ItemBuilder(Material.POTION, (short) 8201).setDisplayname("§7Trank der Stärke").build());
+        p.getInventory().addItem(new ItemBuilder(Material.POTION, (short) 8227).setDisplayname("§7Trank der Feuerresistenz").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_APPLE).setDisplayname("§7Goldener Apfel").setAmount(8).build());
+        p.getInventory().addItem(new ItemBuilder(Material.ENDER_PEARL).setDisplayname("§7Enderperle").setAmount(8).build());
+
+    }
+    void addDiamantItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_SWORD).setDisplayname("§7Diamantschwert").build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_PICKAXE).setDisplayname("§7Diamantspitzhacke").build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_AXE).setDisplayname("§7Diamantaxt").build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_SPADE).setDisplayname("§7Diamantschaufel").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_CARROT).setDisplayname("§7Goldene Karotten").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_HELMET).setDisplayname("§7Diamanthelm").build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_CHESTPLATE).setDisplayname("§7Diamantbrustplatte").build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_LEGGINGS).setDisplayname("§7Diamanthose").build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_BOOTS).setDisplayname("§7Diamantschuhe").build());
+    }
+    void addBergarbeiterItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_CARROT).setDisplayname("§7Goldene Karotten").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_PICKAXE).setDisplayname("§7Diamantspitzhacke").addEnchant(Enchantment.DIG_SPEED, 5, false).build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_BLOCK).setDisplayname("§7Diamantblock").setAmount(2).build());
+    }
+    void addGoldfingerItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_CARROT).setDisplayname("§7Goldene Karotten").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_BLOCK).setDisplayname("§7Goldblock").setAmount(3).build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_APPLE).setDisplayname("§7Goldener Apfel").setAmount(4).build());
+        p.getInventory().addItem(new ItemBuilder(Material.EXP_BOTTLE).setDisplayname("§7Erfahrungsflasche").setAmount(5).build());
+        p.getInventory().addItem(new ItemBuilder(Material.EXP_BOTTLE).setDisplayname("§6§lSchool XP")
+                .setLore("§8» §7Beinhaltet:§6§l 1200 XP").build());
+    }
+    void addJuwelierItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_CARROT).setDisplayname("§7Goldene Karotten").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.DIAMOND_BLOCK).setDisplayname("§7Diamantblock").setAmount(5).build());
+        p.getInventory().addItem(new ItemBuilder(Material.EMERALD_BLOCK).setDisplayname("§7Smaragdblock").build());
+        p.getInventory().addItem(new ItemBuilder(Material.EXP_BOTTLE).setDisplayname("§6§lSchool XP")
+                .setLore("§8» §7Beinhaltet:§6§l 1200 XP").build());
+    }
+    void addBänkerItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.GOLDEN_CARROT).setDisplayname("§7Goldene Karotten").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.EMERALD_BLOCK).setDisplayname("§7Smaragdblock").setAmount(8).build());
+        p.getInventory().addItem(new ItemBuilder(Material.ENCHANTED_BOOK).setDisplayname("§5Zufällige §5§lVerzauberung")
+                .setLore("§eLevel §e§lI", "§7Rechtsklicke dieses Buch", "§7um eine §6§lzufällige Verzauberung", "§7zu erhalten").build());
+        p.getInventory().addItem(new ItemBuilder(Material.EXP_BOTTLE).setDisplayname("§6§lSchool XP")
+                .setLore("§8» §7Beinhaltet:§6§l 1200 XP").build());
+    }
+    void addNinjaItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.MELON).setDisplayname("§7Melone").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_PICKAXE).setDisplayname("§2Bambus Häcksler").addEnchant(Enchantment.DIG_SPEED, 4, false)
+                .addEnchant(Enchantment.DURABILITY, 10, true).setLore("§6§lDuplizierung II").build());
+        p.getInventory().addItem(new ItemBuilder(Material.EXP_BOTTLE).setDisplayname("§6§lSchool XP")
+                .setLore("§8» §7Beinhaltet:§6§l 1200 XP").build());
+        p.getInventory().addItem(new ItemBuilder(Material.MELON_BLOCK).setDisplayname("§7Melonenblock").setAmount(8).build());
+    }
+    void addSenseiItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.MELON).setDisplayname("§7Melone").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_PICKAXE).setDisplayname("§2Bambus-Kapfstab eines Ninjas").addEnchant(Enchantment.DAMAGE_ALL, 6, true)
+                .addEnchant(Enchantment.KNOCKBACK, 1, false).setLore("§c§lKopfgeld II").build());
+        p.getInventory().addItem(new ItemBuilder(Material.EXP_BOTTLE).setDisplayname("§6§lSchool XP")
+                .setLore("§8» §7Beinhaltet:§6§l 1200 XP").setAmount(3).build());
+        p.getInventory().addItem(new ItemBuilder(Material.MELON_BLOCK).setDisplayname("§7Melonenblock").setAmount(20).build());
+    }
+    void addMeisterItems(Player p) {
+
+        p.getInventory().addItem(new ItemBuilder(Material.MELON).setDisplayname("§7Melone").setAmount(12).build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_HELMET).setDisplayname("§2Helm eines Ninjas").addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, false)
+                .addEnchant(Enchantment.DURABILITY, 10, true).setLore("§a§lGlückpilz II").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_CHESTPLATE).setDisplayname("§2Robe eines Ninjas").addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, false)
+                .addEnchant(Enchantment.DURABILITY, 10, true).setLore("§a§lGlückpilz II").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_LEGGINGS).setDisplayname("§2Hose eines Ninjas").addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, false)
+                .addEnchant(Enchantment.DURABILITY, 10, true).setLore("§a§lGlückpilz II").build());
+        p.getInventory().addItem(new ItemBuilder(Material.GOLD_BOOTS).setDisplayname("§2Schuhe eines Ninjas").addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, false)
+                .addEnchant(Enchantment.DURABILITY, 10, true).setLore("§a§lGlückpilz II").build());
+
+        p.getInventory().addItem(new ItemBuilder(Material.MELON_BLOCK).setDisplayname("§7Melonenblock").setAmount(35).build());
+        p.getInventory().addItem(new ItemBuilder(Material.ENCHANTED_BOOK).setDisplayname("§5Zufällige §5§lVerzauberung")
+                .setLore("§eLevel §e§lI", "§7Rechtsklicke dieses Buch", "§7um eine §6§lzufällige Verzauberung", "§7zu erhalten").build());
     }
 
 }
